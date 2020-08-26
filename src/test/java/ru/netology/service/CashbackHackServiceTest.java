1a03c7a4e6946a3d0b50fcc867206058e24a1044
Sofia Larina <zazaq97@gmail.com>
2020-08-26 14:57:54 +0300

    junit 4 added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,7 +1,7 @@
 package ru.netology.service;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.junit.Test;
+import org.junit.Assert;
 
 public class CashbackHackServiceTest {
     public CashbackHackService service = new CashbackHackService();
